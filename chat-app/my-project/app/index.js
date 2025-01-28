896ab08ee4647e139e50939adfe1ba8326c81e6a
tuyishimehono <tuhonori1@gmail.com>
2025-01-28 13:10:01 +0200

    refactor: adjust socket connection message

--- chat-app/my-project/app/index.js
+++ chat-app/my-project/app/index.js
@@ -11,9 +11,12 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+  //   console.log("a user connected");
+  //   socket.on("disconnect", () => {
+  //     console.log("user disconnected");
+  //   });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
 
