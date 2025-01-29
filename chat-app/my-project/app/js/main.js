5b6f4f5292950cbf5ddeca3fa04a8e87a7a679d6
tuyishimehono <tuhonori1@gmail.com>
2025-01-29 15:57:20 +0200

    add timestamp

--- chat-app/my-project/app/js/main.js
+++ chat-app/my-project/app/js/main.js
@@ -5,13 +5,14 @@ let input = document.getElementById("input");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (input.value) {
+    const timestamp = new Date().toLocaleTimeString();
     socket.emit("chat message", input.value);
     input.value = "";
   }
 });
 
-socket.on("chat message", function (msg) {
+socket.on("chat message", function (data) {
   let item = document.createElement("li");
-  item.textContent = msg;
+  item.textContent = `${data.timestamp} - ${data.message}`;
   document.getElementById("messages").appendChild(item);
 });
