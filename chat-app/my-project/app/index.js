4eac709009d460357bf7fd3f34a1bfa7571fa9cf
tuyishimehono <tuhonori1@gmail.com>
2025-01-29 15:57:29 +0200

    adjust chat message

--- chat-app/my-project/app/index.js
+++ chat-app/my-project/app/index.js
@@ -11,12 +11,8 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  //   console.log("a user connected");
-  //   socket.on("disconnect", () => {
-  //     console.log("user disconnected");
-  //   });
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("chat message", (data) => {
+    io.emit("chat message", data);
   });
 });
 
