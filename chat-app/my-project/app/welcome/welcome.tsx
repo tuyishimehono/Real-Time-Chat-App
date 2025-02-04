65a09e0cc4db39b1611d81db3b03278653199462
tuyishimehono <tuhonori1@gmail.com>
2025-02-04 15:47:34 +0200

    remove unnecessary imports

--- chat-app/my-project/app/welcome/welcome.tsx
+++ chat-app/my-project/app/welcome/welcome.tsx
@@ -1,6 +1,3 @@
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
