1157c7591e0c622ed5e17f9e259419600440e9fd
tuyishimehono <tuhonori1@gmail.com>
2025-02-11 20:54:00 +0200

    style: adjust homepage layout

--- chat-app/my-project/app/welcome/welcome.tsx
+++ chat-app/my-project/app/welcome/welcome.tsx
@@ -1,10 +1,9 @@
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div>
+      <div className="h-60 bg-blue-600">
         <h1>{`<>DevRooms</>`}</h1>
         <input placeholder="Username..." />
-
         <select>
           <option>-- Select Room --</option>
           <option value="javascript">JavaScript</option>
